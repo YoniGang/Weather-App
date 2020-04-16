113ed92e9ee573f5da902a48af4773a2a7b5c65c
Yoni Gang <yonigang1@gmail.com>
2020-04-16 12:17:25 +0300

    create hbs instead of html

--- src/app.js
+++ src/app.js
@@ -3,7 +3,28 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Yoni Gang'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Yoni Gang'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        msg: 'This is help'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
